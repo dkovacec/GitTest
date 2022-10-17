f1e8d29bdce320ccc3ed9e3ff1bd17d38c6ede30
Dalibor Kovačec <dalibor.kovacec@zg.ht.hr>
2022-10-17 14:24:25 +0200

    Second commit - version 1.01

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+            System.out.println("Hello world!");
+
+        Person firstPerson = new Person("Petar", "Hrvatska");
+        Person secondPerson = new Person("Joža", "Hrvatska");
+        System.out.println(firstPerson.getName());
+
+
+    }
+
+
+}
